5dc853e0e15cef5d7dbdc77508d0271339db6d59
pioter <51341173+jjanuszex@users.noreply.github.com>
2024-04-17 09:29:42 +0200

    Update 3-argocd.tf

--- kubernetes-addons/3-argocd.tf
+++ kubernetes-addons/3-argocd.tf
@@ -1,23 +1,14 @@
-variable "enable_argocd" {
-  description = "Determines whether to deploy argocd"
-  type        = bool
-  default     = false
-}
-
-variable "argocd_helm_verion" {
-  description = "argocd Helm verion"
-  type        = string
-}
-
-
 resource "helm_release" "argocd" {
+  count = var.enable_cluster_autoscaler ? 1 : 0
+
   name = "argocd"
 
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
   create_namespace = true
-  version          = "3.35.4"
+  version          = var.cluster_autoscaler_helm_verion
 
   values = [file("values/argocd.yaml")]
+
 }

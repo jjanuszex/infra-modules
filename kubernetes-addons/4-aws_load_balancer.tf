2449662d9dcb5b2b1435851636bbdc8557ff085d
Jan Januszex <jjanuszex@gmail.com>
2024-04-19 17:31:26 +0200

    Add clusterName to helm_release aws_lb_controller

--- kubernetes-addons/4-aws_load_balancer.tf
+++ kubernetes-addons/4-aws_load_balancer.tf
@@ -53,6 +53,11 @@ resource "helm_release" "aws_lb_controller" {
   }
 
   set {
+    name  = "clusterName"
+    value = aws_eks_cluster.this.name
+  }
+
+  set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = aws_iam_role.aws_load_balancer_controller.arn
   }
